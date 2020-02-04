5fb5aecaf982f8ef804d43272b5f24aff051c488
kieran-grant <2357351g@student.gla.ac.uk>
2020-02-04 17:45:44 +0000

    Added Category dictionary to index

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 
 def index(request):
-    # Construct a dictionary to pass to the template engine as its context
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    # Query the database for a list of all categories currently stored.
+    # Order the categories by the number of likes in decending order.
+    # Retrieve the top 5 -- or all if less than 5.
+    # Place the list in context_dict
+    category_list = Category.objects.order_by('-likes')[:5]
 
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
     # Return a rendered reponse to send to the client
     return render(request, 'rango/index.html', context=context_dict)
 
