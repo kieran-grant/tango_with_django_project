0cf589b662bfc03aec3f95f5b470e802d3be044a
kieran-grant <2357351g@student.gla.ac.uk>
2020-02-04 18:39:48 +0000

    Chapter 6 complete untill exercises

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
+from rango.models import Page
 
 def index(request):
     # Query the database for a list of all categories currently stored.
@@ -12,14 +13,33 @@ def index(request):
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+
     # Return a rendered reponse to send to the client
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    #return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
-
-    # Construct a dictionary to pass to the template engine as its context
-    # context_dict = {'boldmessage': 'This tutorial has been put together by Kieran Grant.'}
-
-    # Return a rendered reponse to send to the client
     return render(request, 'rango/about.html')
+
+def show_category(request, category_name_slug):
+    # Create a context dictionary which we can pass to the template rendering engine.
+    context_dict = {}
+
+    try:
+        # Either find category_name_slug or raise exception
+        category = Category.objects.get(slug=category_name_slug)
+
+        # Get assosiated pages
+        pages = Page.objects.filter(category=category)
+
+        #Add results list to the template context under name pages
+        context_dict['pages'] = pages
+
+        # Also add the category object from the database to context dictionary
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context=context_dict)
+        
